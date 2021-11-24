ed600eb48d45b715a74a3e006bb69091bb0dc913
isis <icey1727@gmail.com>
2021-11-24 16:30:17 -0500

    edits

--- src/App.vue
+++ src/App.vue
@@ -63,14 +63,6 @@
                 <router-link to="/login" class="nav-link"> Login </router-link>
               </li>
             </ul>
-            <li v-if="!isLoggedIn()" class="pl-4 d-none d-lg-inline-block">
-              <router-link
-                to="/signup"
-                class="btn btn-styled btn-sm btn-base-1 btn-circle"
-              >
-                Signup
-              </router-link>
-            </li>
           </div>
           <div v-if="!isLoggedIn()" class="pl-4 d-none d-lg-inline-block">
             <router-link
